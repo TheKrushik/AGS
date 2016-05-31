9f27a41e3372dc4719ba0db4b4afa95cd7e02b86
krush <krushik@ukr.net>
2016-05-31 15:45:18 +0300

    SignIn+SignUp from SQLite

--- app/src/main/java/info/krushik/android/ags/activity/MainActivity.java
+++ app/src/main/java/info/krushik/android/ags/activity/MainActivity.java
@@ -1,22 +1,36 @@
 package info.krushik.android.ags.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import info.krushik.android.ags.R;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        tvHello = (TextView) findViewById(R.id.tvHello);
+
+        Intent intentLogin = getIntent();
+        String userName = intentLogin.getStringExtra("login");
+
+        tvHello.setText(userName + ", выберите действие...");
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +40,25 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu1:
+                Intent intent = new Intent(this, AboutActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
